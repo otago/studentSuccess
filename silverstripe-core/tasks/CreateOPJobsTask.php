2e3b4e6c9e91d7669fa650f7b6211bf62d9892ba
Alastair Nicholl <alastair.nicholl@op.ac.nz>
2016-05-06 12:09:40 +1200

    clear jobs task

---
+++ silverstripe-core/tasks/CreateOPJobsTask.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Creates the first instances of Otago Polytechnic jobs. These are jobs that
+ * are called repeatedly, on a scheduled basis. It leverages SilverStripes
+ * queuedjobs add on
+ * 
+ * @package cms
+ * @subpackage assets
+ */
+class CreateJobsTask extends BuildTask {
+
+	protected $title = "Create  Scheduled Jobs";
+	protected $description = "Clear ALL jobs and add only OP jobs to queue";
+
+	/**
+	 * submit Application data to EBS
+	 * @param type $request
+	 */
+	public function run($request) {
+		// Note this is MySQL only. A for loop will not work on CWP UAT.
+		$count = DB::query('DELETE FROM `QueuedJobDescriptor` WHERE "JobStatus" <> \'Complete\'')->value();
+		//print_r($count);
+
+
+		
+		echo 'Done! easy ;)';
+
+	}
+
+}
