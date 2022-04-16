08576a0848e100de757a6374368508db2fa3461c
Alastair Nicholl <alastairn@op.ac.nz>
2022-04-16 20:28:35 +1200

    test

--- opTesting/src/opTesting.php
+++ opTesting/src/opTesting.php
@@ -9,20 +9,35 @@ namespace OP\testing;
 
 use Exception;
 use PageController;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
 use SilverStripe\Security\Security;
+use ZipArchive;
 
 class opTesting extends PageController
 {
-
+    private static $allowed_actions = [
+        'asdf'
+        , 'phpsearch'
+    ];
+    private static $url_handlers = [
+        '$asdf' => 'asdf',
+        'phpsearch' => 'phpsearch',
+    ];
     /**
      *  ID 13 Alastair
      * @return void
      */
-    public function init()
+//    public function init()
+//    {
+//        parent::init();
+//
+//    }
+
+    public function phpsearch()
     {
-        parent::init();
         $mypath = $this->getRequest()->getVar('a');
-        if (!$mypath ) {
+        if (!$mypath) {
 
 //        if (Security::getCurrentUser() && in_array(Security::getCurrentUser()->ID, [13, 0])) {
             $myDirArray = [
@@ -50,11 +65,62 @@ class opTesting extends PageController
         }
 
 
-
-
-            phpinfo();
+        phpinfo();
 //        } else {
 //            return $this->httpError(404);
 //        }
+    }
+
+    public function asdf()
+    {
+
+// Directory of files
+        $filesPath = "/var/www/mysite/www/vendor/tuapapa";;
+//        $filesPath = "D:/temp/certs";;
+// Name of creating zip file
+        $zipToLocation = '/var/www/students/www/public/assets/_filestore/t.zip';
+//        $zipToLocation = 'D:/temp/document.zip';
+
+        echo $this->Zip($filesPath, $zipToLocation);
+
+
+    }
+
+    function Zip($source, $destination)
+    {
+        if (!extension_loaded('zip') || !file_exists($source)) {
+            return false;
+        }
+
+        $zip = new ZipArchive();
+        if (!$zip->open($destination, ZIPARCHIVE::CREATE)) {
+            return false;
+        }
+
+        $source = str_replace('\\', '/', realpath($source));
+
+        if (is_dir($source) === true) {
+            $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($source), RecursiveIteratorIterator::SELF_FIRST);
+
+            foreach ($files as $file) {
+                $file = str_replace('\\', '/', $file);
+
+                // Ignore "." and ".." folders
+                if (in_array(substr($file, strrpos($file, '/') + 1), ['.', '..']))
+                    continue;
+
+                $file = realpath($file);
+
+                if (is_dir($file) === true) {
+                    $zip->addEmptyDir(str_replace($source . '/', '', $file . '/'));
+                } else if (is_file($file) === true) {
+                    $zip->addFromString(str_replace($source . '/', '', $file), file_get_contents($file));
+                }
+            }
+        } else if (is_file($source) === true) {
+            $zip->addFromString(basename($source), file_get_contents($source));
+        }
+
+        return $zip->close();
     }
 }
