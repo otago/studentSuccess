902423c6c1bbd1dc3df30483cd42b214368baae1
Alastair Nicholl <alastairn@op.ac.nz>
2022-11-03 11:43:37 +1300

    test

--- samladfs/src/Helper/SAMLHelperFunctions.php
+++ samladfs/src/Helper/SAMLHelperFunctions.php
@@ -72,11 +72,12 @@ class SAMLHelperFunctions
         $idpconfig = SAMLHelperFunctions::IDPConfig();
         $SamlConfig = SAMLHelperFunctions::SamlConfig();
         echo "0";
+        die();
         $filepath = ASSETS_DIR.SAMLHelperFunctions::config()->get('filepath');
         if (!file_exists(dirname($filepath))) {
             Filesystem::makeFolder($filepath);
         }
-        die();
+
         echo "1";
         die();
         $retval = [];
