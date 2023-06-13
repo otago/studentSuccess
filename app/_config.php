1066c210011205af76c34df49b3082d48cf22aca
Alastairkn <alastairkn@gmail.com>
2023-06-14 09:53:03 +1200

    remove elemetne table (#30)
    Co-authored-by: Alastair Nicholl <alastairn@op.ac.nz>

--- app/_config.php
+++ app/_config.php
@@ -4,19 +4,13 @@ use OP\Studentsuccess\ElementFile;
 use OP\Studentsuccess\ElementImage;
 use OP\Studentsuccess\ElementLink;
 use SilverStripe\i18n\i18n;
-use SilverStripe\Control\Controller;
 use SilverStripe\Core\Config\Config;
-use OP\Studentsuccess\LandingPage;
 use OP\Studentsuccess\MasonryContent;
-use OP\Studentsuccess\LandingSearchPage;
 use OP\Studentsuccess\MasonryContentsWithFilters;
 use OP\Studentsuccess\HomePage;
 use OP\Studentsuccess\WayFinder;
 use OP\Studentsuccess\HearFromOthers;
-use OP\Studentsuccess\AccordionPage;
-use OP\Studentsuccess\Accordion;
 use OP\Studentsuccess\AccordionItem;
-use OP\Studentsuccess\ElementTable;
 use OP\Studentsuccess\SidebarImageElement;
 use OP\Studentsuccess\SidebarHelp;
 use OP\Studentsuccess\CTAElement;
@@ -41,23 +35,18 @@ use SilverStripe\ORM\Search\FulltextSearchable;
 global $project;
 $project = 'opt';
 
-
-//global $database;
-//$database = (defined('SS_DATABASE_NAME')) ? SS_DATABASE_NAME : 'op3';
-
-//require_once("conf/ConfigureFromEnv.php");
 i18n::set_locale('en_NZ');
 ini_set('date.timezone', 'Pacific/Auckland');
 FulltextSearchable::enable();
 
 //SS_Log::add_writer(new SS_LogEmailWriter('alastairn@op.ac.nz'), SS_Log::WARN, '<=');
-$arrStyles = array(
+$arrStyles = [
     'Large' => 'large',
     'Feature Link' => 'feature-link',
     'Feature Download' => 'feature-download',
     'Feature Email' => 'feature-mail',
     'Feature External Link' => 'feature-external-link'
-);
+];
 
 $strItems = '';
 foreach ($arrStyles as $strKey => $strCSS) {
@@ -65,37 +54,15 @@ foreach ($arrStyles as $strKey => $strCSS) {
 }
 $strItems = substr($strItems, 0, -1);
 
-//Config::inst()->remove(LandingPage::class, 'allowed_elements');
-//
-//Config::inst()->update(LandingPage::class, 'allowed_elements', array(
-//    MasonryContent::class
-//));
-//
-//Config::inst()->remove(LandingSearchPage::class, 'allowed_elements');
-//
-//Config::inst()->update(LandingSearchPage::class, 'allowed_elements', array(
-//    MasonryContentsWithFilters::class
-//));
-//
 Config::inst()->remove(HomePage::class, 'allowed_elements');
-//
-Config::inst()->update(HomePage::class, 'allowed_elements', array(
+
+Config::inst()->update(HomePage::class, 'allowed_elements', [
     WayFinder::class,
     HearFromOthers::class
-));
-//
-//Config::inst()->remove(AccordionPage::class, 'allowed_elements');
-//
-//Config::inst()->update(AccordionPage::class, 'allowed_elements', array(
-//    Accordion::class
-//));
-//
-//Config::inst()->remove('FilterableChecklist', 'allowed_elements');
-//
-//Config::inst()->remove(AccordionItem::class, 'allowed_elements');
+]);
 
-$classes = array(
-    ElementTable::class,
+
+$classes = [
     ElementFile::class,
     ElementLink::class,
     ElementImage::class,
@@ -118,13 +85,13 @@ $classes = array(
     MatrixElement::class,
     SidebarTestimony::class,
     TabbedCheckList::class
-);
+];
 
 
 Config::inst()->update(AccordionItem::class, 'allowed_elements', $classes);
 
-Config::inst()->update('Page', 'allowed_elements', array(
+Config::inst()->update('Page', 'allowed_elements', [
     MasonryContent::class,
     MasonryContentsWithFilters::class,
     TabbedCheckList::class
-));
+]);
